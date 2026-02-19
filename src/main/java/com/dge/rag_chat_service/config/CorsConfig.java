4a3ab347a61726c6cc66aa2ef6cf70f1be7bb3b8
binaygaur <binay.gaur@nagarro.com>
2026-02-19 12:09:31 +0530

    RAG chat storage service related changes

--- src/main/java/com/dge/rag_chat_service/config/CorsConfig.java
+++ src/main/java/com/dge/rag_chat_service/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
